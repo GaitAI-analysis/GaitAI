0e8a2477b123875397eed15ee8b96c05cf02b051
Naveen297 <naveenmalhotra148@gmail.com>
2026-06-25 15:59:40 +0530

    Deploy frontend-v1 to GitHub Pages 25th june nav

---
+++ _next/static/chunks/app/products/page-f4467d4c7cc6b36f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[286],{5701:function(e,n,s){Promise.resolve().then(s.t.bind(s,2972,23)),Promise.resolve().then(s.bind(s,4006)),Promise.resolve().then(s.bind(s,3377))}},function(e){e.O(0,[779,236,686,866,6,971,117,744],function(){return e(e.s=5701)}),_N_E=e.O()}]);
